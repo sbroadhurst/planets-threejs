50e5fb8dacc5155699b394afb43dec302199c35d
sbroadhurst <stepbro31@gmail.com>
2023-08-08 14:38:16 -0400

    demo cubes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+bootstrapApplication(AppComponent);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  // declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  declarations: [],
+  // bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
