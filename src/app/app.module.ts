9d3ae4b02ac1c19d14a3762b689e12d988d7337d
sbroadhurst <stepbro31@gmail.com>
2023-08-24 12:45:25 -0400

    store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 // import { bootstrapApplication } from '@angular/platform-browser';
 
 // bootstrapApplication(AppComponent);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, StoreModule.forRoot({}, {}), StoreRouterConnectingModule.forRoot()],
   providers: [],
   // declarations: [],
   bootstrap: [AppComponent],
